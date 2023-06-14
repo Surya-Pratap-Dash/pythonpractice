48b95cab37d07a245b34f1d37638ff1a4d269cf6
Surya-Pratap-Dash <suryadashpratap.dash008@gmail.com>
2023-06-14 15:18:31 +0530

    commit these changes

---
+++ OOPS/ Interface Segregation Principle/Interface_Segregation_Principle.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Movable(ABC):
+    @abstractmethod
+    def go(self):
+        pass
+
+class Flyable(Movable):
+    def fly(self):
+        pass
+
+class Aircraft(Flyable):
+    def go(self):
+        print("Taxiing")
+
+    def fly(self):
+        print("Flying")
+
+class Car(Movable):
+    def go(self):
+        print("Going")
+
+
+
