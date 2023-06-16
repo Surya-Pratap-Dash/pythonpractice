0bee4aeb4ed4cd611fcf369a643afce960f7341b
Surya-Pratap-Dash <suryadashpratap.dash008@gmail.com>
2023-06-16 21:43:22 +0530

    commit these changes

---
+++ OOPS/Type class/Type class.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greeting(self):
+        return f'Hi, I am {self.name}. I am {self.age} year old.'
+
+print(type(Person))
+print(isinstance(Person, type))
+
+class_body = """
+def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+def greeting(self):
+    return f'Hi, I am {self.name}. I am {self.age} year old.'
+"""
+
+class_dict = {}
+exec(class_body, globals(), class_dict)
+Person = type('Person', (object,), class_dict)
+print(isinstance(Person, type))
+print(Person.__dict__)
+
+
