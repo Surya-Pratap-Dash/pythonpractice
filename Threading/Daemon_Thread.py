9fd3e2bb51064743dc7aad61af684a3fb50b6922
Surya-Pratap-Dash <suryadashpratap.dash008@gmail.com>
2023-06-21 16:08:43 +0530

    commit these changes

---
+++ Threading/Daemon_Thread.py
@@ -0,0 +1,14 @@
+from threading import Thread
+import time
+
+def show_timer():
+    count = 0
+    while True:
+        count += 1
+        time.sleep(1)
+        print(f'Has beem waiting for {count} second(s)...')
+
+t = Thread(target=show_timer, daemon=True)
+t.start()
+
+answer = input('Do you want to exit?\n')
